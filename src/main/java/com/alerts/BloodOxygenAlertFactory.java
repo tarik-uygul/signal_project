027a8c8a651281b322744dd85039d4f84443cf72
tarikuygul <tarikuygul@gmail.com>
2026-05-19 13:33:51 +0200

    feedback part 4 / added test screenshot

--- src/main/java/com/alerts/BloodOxygenAlertFactory.java
+++ src/main/java/com/alerts/BloodOxygenAlertFactory.java
@@ -3,6 +3,6 @@ package com.alerts;
 public class BloodOxygenAlertFactory extends AlertFactory {
     @Override
     public Alert createAlert(String patientId, String condition, long timestamp) {
-        return new BasicAlert(patientId, condition, timestamp);
+        return new BloodOxygenAlert(patientId, condition, timestamp);
     }
 }
--- src/main/java/com/alerts/BloodPressureAlertFactory.java
+++ src/main/java/com/alerts/BloodPressureAlertFactory.java
@@ -3,6 +3,6 @@ package com.alerts;
 public class BloodPressureAlertFactory extends AlertFactory {
     @Override
     public Alert createAlert(String patientId, String condition, long timestamp) {
-        return new BasicAlert(patientId, condition, timestamp);
+        return new BloodPressureAlert(patientId, condition, timestamp);
     }
 }
